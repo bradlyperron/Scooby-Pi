5e5226594debc86d82103221c7318dfbe3d65735
evan maier <evanm413r@gmail.com>
2019-03-06 00:29:13 +0000

    fixed bug

--- button.py
+++ button.py
@@ -5,11 +5,11 @@ import os
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.IN)
 
-button = GPIO.input(26)
-
 while True:
-	if button == 1:
-		time.sleep(1)
-		if button == 1:
-			os.system("shutdown now -h")
-	time.sleep(1)
+    button = GPIO.input(26)
+    
+    if button == 0:
+        time.sleep(1)
+        if button == 0:
+            os.system("shutdown now -h")
+    time.sleep(1)
