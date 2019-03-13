f43f7716d69f8aaee6309760b380bafef1aee024
evan <evanm413r@gmail.com>
2019-03-13 09:43:48 -0700

    added date and time to log

--- button.py
+++ button.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 import os
 
 GPIO.setwarnings(False)
@@ -13,6 +14,7 @@ while True:
         time.sleep(1)
         if button == True:
             with open("/home/pi/Documents/programs/rpi-programs/button_log.txt","a") as logf:
-                logf.write("button shutdown\n")
+                logf.write("%s" % datetime.now())
+                logf.write("\tbutton shutdown\n")
             #os.system("shutdown now -h")
     time.sleep(1)
