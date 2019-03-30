669dcbd55d31faa668d22dfaaaee36b883d8e52c
evan <evanm413r@gmail.com>
2019-03-29 17:06:17 -0700

    sensor tcp networkinggit add -A!

--- tcp_server.py
+++ tcp_server.py
@@ -1,38 +1,43 @@
 import socket
 import sys
+import multiprocessing
+import struct
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create ipv4 tcp socket
-except socket.error:
-    print("Failed to connect")
-    sys.exit()
+def tcp_server_main(vin):
+    print("server starting")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create ipv4 tcp socket
+    except socket.error:
+        print("Failed to connect")
+        sys.exit()
 
-print("Socket Created")
+    print("Socket Created")
 
-server_address = ('localhost',8888) #specify host and port 
+    server_address = ('192.168.0.21', 8888) #specify host and port 
 
-try:
-    s.bind(server_address)
-except socket.error:
-    print("Binding failed")
-    sys.exit()
+    try:
+        s.bind(server_address)
+    except socket.error:
+        print("Binding failed")
+        sys.exit()
 
-print("Socked has been bounded")
+    print("Socked has been bounded")
 
-s.listen(2)
+    s.listen(2)
 
-print("Socket is ready")
+    print("Socket is ready")
 
-conn, addr = s.accept()
+    conn, addr = s.accept()
 
-print("Connected with {} : {}".format(addr[0], str(addr[1])))
+    print("Connected with {} : {}".format(addr[0], str(addr[1])))
 
-while True:
-    data = conn.recv(1024) # recieve 1 kb of data at a time
-    print("OK. {}".format(data.decode())) # print decoded data in string form
-    if not data: # if connection is broken, break
-        break
-    conn.sendall(data) # send all data in one packet
+    while True:
+        request = conn.recv(1024) # check if client wants data
+        #print(vin.value) # voltage from adc
+        if not request: # if connection is broken, break
+            break
+        conn.sendall(bytearray(struct.pack("f", vin.value))) # send all data in one packet
 
-conn.close() #close connection
-s.close() #close socket
+    conn.close() #close connection
+    s.close() #close socket
+    print("server stopping")
