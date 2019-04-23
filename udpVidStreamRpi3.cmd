0c538dcc5e8753ebe24c398947cec4089e40b438
emaier <evanm413r@gmail.com>
2019-04-23 12:05:18 -0700

    Delete udpVidStreamRpi3.cmd

--- udpVidStreamRpi3.cmd
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 -v udpsrc port=5002 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264" ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
