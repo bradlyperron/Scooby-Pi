ebbc7472034329aec84aba610264f7a8f0bf73fb
evan <evanm413r@gmail.com>
2019-04-23 12:11:04 -0700

    suh

--- adc.py
+++ adc.py
@@ -3,8 +3,9 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import numpy as np
 
-def adc_main(vin,amp):
+def adc_main(vin,amp,prim_amp):
     print("adc starting")
     # (R1 + R2)/R2
 
@@ -17,18 +18,27 @@ def adc_main(vin,amp):
     ads = ADS.ADS1115(i2c)
 
     # Create single-ended input on channel 0
-    chan0 = AnalogIn(ads, ADS.P0)
-    chan1 = AnalogIn(ads, ADS.P1)
+    chan0 = AnalogIn(ads, ADS.P0) #voltage
+    chan1 = AnalogIn(ads, ADS.P1) #primary amp
+    chan2 = AnalogIn(ads, ADS.P2) #secondary amp
 
+    offset = 0
+    scale = 1
     while True:
         #calculate input voltage and amps
-        vin.value = chan1.voltage*resistor_ratio
+        vin.value = chan0.voltage*resistor_ratio
+        
         i=0
-        val=[0]*5
+        amp_val = prim_amp_val = [0]*5
+        
         for i in range(5):
-            val[i]=chan0.value
+            prim_amp_val[i] = (chan1.value-offset)/scale
+            amp_val[i] = (chan2.value-offset)/scale
             time.sleep(0.5)
-        vals = (val[0]+val[1]+val[2]+val[3]+val[4])/5
-        amp.value = (vals-19080)/(530.0)
-        time.sleep(0.5)
+        
+        amp.value = np.mean(amp_val)
+        prim_amp.value = np.mean(prim_amp_val)
+        
+        print("{}, {}".format(amp.value,prim_amp.value))
+        
     print("adc stopping")
--- single_ended_adc.py
+++ single_ended_adc.py
@@ -15,22 +15,25 @@ ads = ADS.ADS1115(i2c)
 
 ads.gain = 1
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+chan0 = AnalogIn(ads, ADS.P0)
 chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
 # Create differential input between channel 0 and 1
 #chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
-print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format('raw', 'a', 'raw', 'v'))
+print("{:>5}\t{:>5}\t{:>5}".format('v1','v2','v3'))
+#print("{:>5}\t{:>5}".format('raw', 'v'))
 
 while True:
-    v = chan1.voltage*resistor_ratio
-    i=0
-    val=[0]*5
-    for i in range(5):
-        val[i]=chan.value
-        time.sleep(0.5)
-    vals = (val[0]+val[1]+val[2]+val[3]+val[4])/5
-    amps = (vals-19080)/(530.0)
-    print("{:>5.0f}\t{:>5.1f}\t{:>5.3f}\t{:>5.1f}".format(vals,amps,chan1.voltage,v))
+    #v = chan1.voltage*resistor_ratio
+    #i=0
+    #val=[0]*5
+    #for i in range(5):
+     #   val[i]=chan.value
+      #  time.sleep(0.5)
+    #vals = (val[0]+val[1]+val[2]+val[3]+val[4])/5
+    #amps = (vals-20350)/(400.0)
+    print("{:>5.3f}\t{:>5.3f}\t{:>5.3f}".format(chan0.voltage,chan1.voltage,chan2.voltage))
+    #print("{:>5.3f}\t{:>5.1f}".format(chan1.voltage,v))
     time.sleep(0.5)
 
