93f0bef1c3ffc54dfee35710715fa9736540278b
emaier <evanm413r@gmail.com>
2019-04-23 12:02:15 -0700

    Delete launch UAV.bat

--- launch UAV.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM launch UAVcast-pro Webpages
-SET BROWSER=chrome.exe
-SET WAIT_TIME=2
-start chrome -new-window http://172.29.136.112
-start chrome -new-window http://172.29.119.8
-start chrome -new-window http://172.29.30.168
-
-exit 
