9f2e23835223ecaa6b88fbc86787c7fc05ecdb62
emaier <evanm413r@gmail.com>
2019-05-09 12:07:31 -0700

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -3,7 +3,7 @@ import sys
 import multiprocessing
 import struct
 
-def tcp_server_main(vin,amp,prim_amp):
+def tcp_server_main(volt1,volt2,motor_amp,actuator_amp):
     print("server starting")
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create ipv4 tcp socket
@@ -37,16 +37,19 @@ def tcp_server_main(vin,amp,prim_amp):
     print("Connected with {} : {}".format(addr[0], str(addr[1])))
     
     while True:
-        volts = str(vin.value)
-        amps = str(amp.value)
-        prim_amps = str(prim_amp.value)
+        v1 = str(volt1.value)
+        v2 = str(volt2.value)
+        motor_amps = str(motor_amp.value)
+        actuator_amps = str(actuator_amp.value)
         request = conn.recv(1024).decode()
-        if request == "voltage":
-            conn.sendall(volts.encode()) # send all data in one packet
-        elif request == "current":
-            conn.sendall(amps.encode())
-        elif request == "prim_current":
-            conn.sendall(prim_amps.encode())
+        if request == "volt1":
+            conn.sendall(v1.encode()) # send all data in one packet
+        elif request == "volt2":
+            conn.sendall(v2.encode())
+        elif request == "motor current":
+            conn.sendall(motor_amps.encode())
+        elif request == "actuator current":
+            conn.sendall(actuator_amps.encode())
         elif request:
             print("invalid request")
         else:
