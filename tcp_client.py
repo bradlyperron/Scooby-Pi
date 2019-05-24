00db8ff0886c6d794ca8cd4b41b8c9b66aa752fc
emaier <evanm413r@gmail.com>
2019-05-24 13:08:20 -0700

    remove threading

--- tcp_client.py
+++ tcp_client.py
@@ -6,84 +6,93 @@ import msvcrt
 import threading
 
 def tcp_client_main(volt1,volt2,actuator_amp,motor_amp):
-    def worker():
-
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create ipv4 tcp socket
-        except socket.error:
-            print("Failed to connect")
-            sys.exit()
-
-        print("Socket Created")
-
-        server_address = ('172.29.93.7',5005) # specify host and port
-        #server_address = ('192.168.0.16', 8888) 
-        (host, port) = server_address # unpack tuple
-
-        s.connect(server_address) # connect socket to port where server is listening
-        #s.close()
-
-        print("Socket Connected {} to using IP {}".format(host, port))
-
-        while not event.is_set():
-            request = "volt1" 
-            try:
-                s.sendall(request.encode()) # send request in byte array
-            except socket.error:
-                print("Did not send successfully")
-                sys.exit()
-
-            reply = s.recv(4096) # recieve reply from server
-
-            volt1.value = float(reply.decode()) # unpack byte array inside tuple
-            time.sleep(1)
-
-            request = "volt2" 
-            try:
-                s.sendall(request.encode()) # send request in byte array
-            except socket.error:
-                print("Did not send successfully")
-                sys.exit()
-
-            reply = s.recv(4096) # recieve reply from server
-
-            volt2.value = float(reply.decode()) # unpack byte array inside tuple
-            time.sleep(1)
-
-            request = "actuator current"
-            try:
-                s.sendall(request.encode()) # send request in byte array
-            except socket.error:
-                print("Did not send successfully")
-                sys.exit()
-
-            reply = s.recv(4096) # recieve reply from server
-            actuator_amp.value = float(reply.decode()) # unpack byte array inside tuple
-            time.sleep(1)
-            
-            request = "motor current"
-            try:
-                s.sendall(request.encode()) # send request in byte array
-            except socket.error:
-                print("Did not send successfully")
-                sys.exit()
-
-            reply = s.recv(4096) # recieve reply from server
-            motor_amp.value = float(reply.decode()) # unpack byte array inside tuple
-            #print("{:>3.1f}\t{:>5.1f}".format(motor_amp.value,actuator_amp.value))
-            time.sleep(1)
-
-        print("closing socket") # close socket
-        s.close()
+#def worker():
         
-    def flag(): # wait for keyboard interrupt
-        if msvcrt.getch():
-            event.set()
+    #create socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create ipv4 tcp socket
+    except socket.error:
+        print("Failed to connect")
+        sys.exit()
 
-    event = threading.Event()
+    print("Socket Created")
+    # specify host and port
+    server_address = ('172.29.93.7',5005)
+    # unpack tuple
+    (host, port) = server_address
+    # connect socket to port where server is listening
+    s.connect(server_address) 
 
-    tflag = threading.Thread(target=flag)
-    tworker = threading.Thread(target=worker)
+    print("Socket Connected {} to using IP {}".format(host, port))
 
-    tflag.start()
-    tworker.start()
+    #while not event.is_set():
+    while true:
+        request = "volt1" 
+        try:
+            # send request in byte array
+            s.sendall(request.encode()) 
+        except socket.error:
+            print("Did not send successfully")
+            sys.exit()
+        # recieve reply from server
+        reply = s.recv(4096) 
+        # unpack byte array inside tuple
+        volt1.value = float(reply.decode()) 
+        time.sleep(1)
+
+        request = "volt2" 
+        try:
+            # send request in byte array
+            s.sendall(request.encode()) 
+        except socket.error:
+            print("Did not send successfully")
+            sys.exit()
+        # recieve reply from server
+        reply = s.recv(4096) 
+        # unpack byte array inside tuple
+        volt2.value = float(reply.decode()) 
+        time.sleep(1)
+
+        request = "actuator current"
+        try:
+            # send request in byte array
+            s.sendall(request.encode()) 
+        except socket.error:
+            print("Did not send successfully")
+            sys.exit()
+        # recieve reply from server
+        reply = s.recv(4096) 
+        # unpack byte array inside tuple
+        actuator_amp.value = float(reply.decode()) 
+        time.sleep(1)
+
+        request = "motor current"
+        try:
+            # send request in byte array
+            s.sendall(request.encode()) 
+        except socket.error:
+            print("Did not send successfully")
+            sys.exit()
+        # recieve reply from server
+        reply = s.recv(4096) 
+        # unpack byte array inside tuple
+        motor_amp.value = float(reply.decode()) 
+        #print("{:>3.1f}\t{:>5.1f}".format(motor_amp.value,actuator_amp.value))
+        time.sleep(1)
+
+    # close socket
+    print("closing socket") 
+    s.close()
+
+#def flag(): 
+    # wait for keyboard interrupt
+ #   if msvcrt.getch():
+  #      event.set()
+
+#event = threading.Event()
+
+#tflag = threading.Thread(target=flag)
+#tworker = threading.Thread(target=worker)
+
+#tflag.start()
+#tworker.start()
