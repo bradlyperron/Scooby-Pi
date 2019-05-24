e37b4830abd770c786db5a026860f8aadfa11320
evan <evanm413r@gmail.com>
2019-05-24 19:38:07 +0100

    updated for pi zero

--- adc.py
+++ adc.py
@@ -7,25 +7,26 @@ import numpy as np
 
 def adc_main(volt1,volt2,motor_amp,actuator_amp):
     print("adc starting")
-    # (R1 + R2)/R2
 
-    resistor_ratio2 = 4.9305
-    resistor_ratio1 = 3.1088
+    # (R1 + R2)/R2
+    resistor_ratio2 = 4.93
+    resistor_ratio1 = 3.04
 
     # Create the I2C bus
     i2c = busio.I2C(board.SCL, board.SDA)
 
     # Create the ADC object using the I2C bus
     ads = ADS.ADS1115(i2c)
+    ads1 = ADS.ADS1115(i2c, address=0x49)
 
-    # Create single-ended input on channel 0
-    chan0 = AnalogIn(ads, ADS.P0) #primary voltage
+    # Create single-ended input on channel 1
+    chan0 = AnalogIn(ads1, ADS.P0, ADS.P1) #primary voltage
     chan1 = AnalogIn(ads, ADS.P1) #secondary voltage
     chan2 = AnalogIn(ads, ADS.P2) #motor current
     chan3 = AnalogIn(ads, ADS.P3) #actuator current
 
-    motor_amp_offset = 2.505
-    actuator_amp_offset = 2.550
+    motor_amp_offset = 2.485 
+    actuator_amp_offset = 2.520 
     scale = 0.04
     while True:
         #calculate input voltage and amps
@@ -40,9 +41,9 @@ def adc_main(volt1,volt2,motor_amp,actuator_amp):
             actuator_amp_val[i] = (chan3.voltage-actuator_amp_offset)/scale
             time.sleep(0.25)
         
-        print("motor amp: {}".format(motor_amp.value))
-        print("actuator amp: {}".format(actuator_amp.value))
         motor_amp.value = np.average(motor_amp_val)
         actuator_amp.value = np.average(actuator_amp_val)
+        #print("motor amp: {}\t actuator amp: {}".format(motor_amp.value,actuator_amp.value))
+        #print("motor amp: {}\t actuator amp: {}".format(chan2.voltage,chan3.voltage))
         
     print("adc stopping")
