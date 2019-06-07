8739375a64b3da1af7931bd390a176ea6af5ddfe
emaier <evanm413r@gmail.com>
2019-06-07 12:47:40 -0700

    Delete launchTcp.cmd

--- launchTcp.cmd
+++
@@ -1,9 +0,0 @@
-cd C:\gstreamer\1.0\x86\bin
-REM launch stream 1
-start tcpVidStreamRpi1.cmd
-REM launch stream 2
-start tcpVidStreamRpi2.cmd
-REM launch stream 3
-start tcpVidStreamRpi3.cmd
-REM launch stream z1
-start tcpVidStreamRpiz1.cmd
