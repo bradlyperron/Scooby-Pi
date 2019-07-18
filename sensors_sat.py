e4991d0039daad586ef10e41d18f85ccf2138603
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-18 11:58:11 -0700

    Update sensors_sat.py

--- sensors_sat.py
+++ sensors_sat.py
@@ -1,50 +1,42 @@
-import time
-import json
-import filehandler
 import rockBlock
-import sys
-import struct
-
+import time
+import filehandler
+import json 
+ 
 from rockBlock import rockBlockProtocol
  
-class Sensors (rockBlockProtocol):
-	
-	SLEEP_INTERVAL = 60
+class Sensor (rockBlockProtocol):
     
+    SLEEP_INTERVAL = 60
+            
     def main(self):
-      
-         = rockBlock.rockBlock("/dev/ttyUSB0", self)
-	
-    while True:
-	
-	value = td.obtain() 
-	
-        self.emit(value)   
+            
+        while(True):
         
-	time.sleep(self.SLEEP_INTERVAL)
-	
-     def emit(self, value):
+            depth = getJson('transducer', 'depth') 
+            
+            self.emit(value)
+                        
+            time.sleep( self.SLEEP_INTERVAL)
+                                
+    def emit(self, value):
             
         rb = rockBlock.rockBlock("/dev/ttyUSB1", self)
         
-        rb.sendMessage("Td:" + str(time.time()) + ":" + str(value) )
+        rb.sendMessage("Td:" + str(time.time()) + ":" + str(depth) )
                                                                                   
         rb.close()
-        
-        
-    def rockBlockTxStarted(self):
-        print "rockBlockTxStarted"
-        
-    def rockBlockTxFailed(self):
-        print "rockBlockTxFailed"
-        
-    def rockBlockTxSuccess(self,momsn):
-        print "rockBlockTxSuccess " + str(momsn)
-        
-if __name__ == '__main__':
-	Sensors().main()
-
 	
+ 
+    def getJson (filename,value):
+        
+        try:
+            data = fileHandler.read('/home/pi/logs/{}.json'.format(filename))
+            data = json.loads(data)
+            return data[value]
+        except:
+            return 0.0
+
 	
 	
 	
