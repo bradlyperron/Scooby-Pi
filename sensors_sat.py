17dcb1cd4da4298e87be8c06a379d5d02b9c6277
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-19 15:14:08 -0700

    Update sensors_sat.py

--- sensors_sat.py
+++ sensors_sat.py
@@ -30,15 +30,15 @@ class Sensor (rockBlockProtocol):
             time.sleep( self.SLEEP_INTERVAL)       
 
     
-            v1 = "{0:.2f}".format(getJson('adc','v1'))
-            v2 = "{0:.2f}".format(getJson('adc','v2'))
-            mc = "{0:.2f}".format(getJson('adc','mc'))
-            ac = "{0:.2f}".format(getJson('adc','ac'))
-            ec = "{0:.2f}".format(getJson('adc','ect'))
-            dpt = getJson('transducer','depth')
-            tmp = getJson('transducer','temperature')
-            dtime = getJson('transducer','time')
-            data = [v1,v2,ac,mc,ec,dpt,tmp,dtime]
+        v1 = "{0:.2f}".format(getJson('adc','v1'))
+        v2 = "{0:.2f}".format(getJson('adc','v2'))
+        mc = "{0:.2f}".format(getJson('adc','mc'))
+        ac = "{0:.2f}".format(getJson('adc','ac'))
+        ec = "{0:.2f}".format(getJson('adc','ect'))
+        dpt = getJson('transducer','depth')
+        tmp = getJson('transducer','temperature')
+        dtime = getJson('transducer','time')
+        data = [v1,v2,ac,mc,ec,dpt,tmp,dtime]
 
         print(data)                                
         #emit(value)
