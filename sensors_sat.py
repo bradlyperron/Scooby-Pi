198d20f85a5365137d7d2fb99c5f3e657370dadf
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-19 15:12:00 -0700

    Update sensors_sat.py

--- sensors_sat.py
+++ sensors_sat.py
@@ -6,33 +6,32 @@ import json
 from rockBlock import rockBlockProtocol
  
 class Sensor (rockBlockProtocol):
-		
+        
     SLEEP_INTERVAL = 60
-	
+    
             
     def main(self):
             
-        while(True):
-		
-	    def getJson (filename,value):
         
-       		try:
-            		data = fileHandler.read('/home/pi/logs/{}.json'.format(filename))
-            		data = json.loads(data)
-            		return data[value]
-        	except:
-            		return 0.0
-		
+        def getJson (filename,value):
+        
+            try:
+                    data = fileHandler.read('/home/pi/logs/{}.json'.format(filename))
+                    data = json.loads(data)
+                    return data[value]
+            except:
+                    return 0.0
+        
           
-	    def emit(value):
-        	rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
-        	rb.sendMessage("Td:" + str(time.time()) + ":" + str(value) )                                                                    
-        	rb.close()
-        	time.sleep( self.SLEEP_INTERVAL)       
+        def emit(value):
+            rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
+            rb.sendMessage("Td:" + str(time.time()) + ":" + str(value) )                                                                    
+            rb.close()
+            time.sleep( self.SLEEP_INTERVAL)       
 
     
-    	    v1 = "{0:.2f}".format(getJson('adc','v1'))
-       	    v2 = "{0:.2f}".format(getJson('adc','v2'))
+            v1 = "{0:.2f}".format(getJson('adc','v1'))
+            v2 = "{0:.2f}".format(getJson('adc','v2'))
             mc = "{0:.2f}".format(getJson('adc','mc'))
             ac = "{0:.2f}".format(getJson('adc','ac'))
             ec = "{0:.2f}".format(getJson('adc','ect'))
@@ -41,9 +40,9 @@ class Sensor (rockBlockProtocol):
             dtime = getJson('transducer','time')
             data = [v1,v2,ac,mc,ec,dpt,tmp,dtime]
 
-	    print(data)	              	               
-	    #emit(value)
-		                             
+        print(data)                                
+        #emit(value)
+                                     
     def rockBlockTxStarted(self):
         print "rockBlockTxStarted"
         
@@ -52,7 +51,8 @@ class Sensor (rockBlockProtocol):
         
     def rockBlockTxSuccess(self,momsn):
         print "rockBlockTxSuccess " + str(momsn)
-	
-	
+    
+    
 if __name__ == '__main__':
-	Sensor().main()
+    Sensor().main()
+
