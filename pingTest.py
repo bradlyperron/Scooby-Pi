ce246c5b54d7d83cb38e28b73421eee1b3b81eca
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-24 13:36:21 -0700

    Update pingTest.py

--- pingTest.py
+++ pingTest.py
@@ -1,3 +1,16 @@
 import subprocess
-state = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
-print(state)
+
+state = ''
+
+def pingTest():
+	ping = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
+	return ping
+
+state = pingTest()
+
+if state == 0:
+	print("The state is Cell")
+else:
+	print("The state is Sat")
+
+
