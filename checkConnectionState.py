20ff525ece2b6fb3157c62102cfb9253bea73cec
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-24 14:14:12 -0700

    Update checkConnectionState.py

--- checkConnectionState.py
+++ checkConnectionState.py
@@ -3,7 +3,7 @@ import time
 
 state = ''
 def pingTest():
-	for i in range(4):
+	for i in range(6):
 		ping = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
 		if ping == 1:
 			return False	
