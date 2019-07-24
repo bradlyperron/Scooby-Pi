409a0c6a3d98d3bbbf542264c337e20a2a8f4a87
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-24 13:54:29 -0700

    Update pingTest.py

--- pingTest.py
+++ pingTest.py
@@ -5,29 +5,30 @@ def pingTest():
 	ping = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
 	return ping
 
+while True:
+	ping = pingTest()
+	print(ping)
+# def checkState():
+# 	state = pingTest()
+# 	if state == 0:
+# 		print("The state is Cell")
+# 		return True
+# 	else:
+# 		for i in range(3):
+# 			val = pingTest()
+# 			print(val)
+# 			if val == 0:
+# 				print("Cell")
+# 				return True
+# 			else:
+# 				print("not connected")
+# 				if i == 2 and val == False:
+# 					return False 
 
-def checkState():
-	state = pingTest()
-	if state == 0:
-		print("The state is Cell")
-		return True
-	else:
-		for i in range(8):
-			val = pingTest()
-			print(val)
-			if val == 0:
-				print("Cell")
-				return True
-			else:
-				print("not connected")
-				if i == 7 and val == False:
-					return False 
 
 
+# if checkConnectionState == True:
+# 	print("Starting cell sensors")
 
-checkConnectionState = checkState() 
-if checkConnectionState == True:
-	print("Starting cell sensors")
-
-else:
-	print("Starting sat sensors")
+# else:
+# 	print("Starting sat sensors")
