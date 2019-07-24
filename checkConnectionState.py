66ae60fad18db19555d5a25cb6537fc3e6058fb1
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-24 14:07:44 -0700

    Update checkConnectionState.py

--- checkConnectionState.py
+++ checkConnectionState.py
@@ -1,41 +1,17 @@
-from subprocess import *
-import sys
 import subprocess
-import os
+import time
 
-identifier = ''
-def ping():
-    try:
-        subprocess.check_output(["ping", "1.1.1.1"])
-        return True                      
-    except subprocess.CalledProcessError:
-        return False
+state = ''
+def pingTest():
+	for i in range(4):
+		num = 0
+		ping = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
+		if ping == 0:
+			num += 1
+	return num
+		else:
+			return False
+	
 
-def check_state():
-	state = ping() 
-	print(state)
-	if state == True:
-		return True
-	else:
-		for i in range(3):
-			val = ping()
-			print(val)		
-			if val == True:
-				return True 
-			else:
-				print("not connected")
-				if i == 2 and val == False:
-					return False
-					
-			
-status = check_state()
-if status == True:
-	#os.system('python3 processes.py')
-	print("starting cell sensors")
-	identifier = 'cell'
-else:
-	#os.system('python satelliteSensors.py')
-	print("starting sat sensors")
-	identifier = 'Sat'
-
-print("the state is", identifier)
+status = pingTest()
+print(status)
