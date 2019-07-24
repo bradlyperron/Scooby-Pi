595d2d7e57fb8f871063e10762b1d36e906776ff
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-24 13:20:49 -0700

    Update pingTest.py

--- pingTest.py
+++ pingTest.py
@@ -1,3 +1,3 @@
 import subprocess
-state = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"], stdout=open(os.devnull,'w'))
+state = subprocess.call(["ping", "8.8.8.8", "-c1", "-W2", "-q"])
 print(state)
